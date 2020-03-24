5af2321fe836371ae071241f1e4d46a78196897e
Madison Volpe <mv1617@nyu.edu>
2020-03-24 11:05:32 -0400

    adding insert sql script

---
+++ scripts/test/insert_into_testcc.sql
@@ -0,0 +1,5 @@
+INSERT INTO cc VALUES
+('New York', 'USA', 'Confirmed', '2019-01-01'),
+('New York', 'USA', 'Confirmed', '2019-01-02'),
+(NULL, 'China', 'Confirmed', '2019-01-01'),
+(NULL, 'China', 'Confirmed', '2019-01-02'); 
